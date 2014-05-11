9262ce8ddfa99023c32cb6e9538434240b351449
Alexandre Pinto <alexcp@gmail.com>
2014-05-11 09:31:26 -0700

    Finished plot1

--- plot1.R
+++ plot1.R
@@ -18,4 +18,15 @@ df.power = read.csv(unz(filename, "household_power_consumption.txt"), header=T,
                                  "numeric", "numeric", "numeric",
                                  "numeric", "numeric", "numeric"))
 
+## Formatting the date and subseting the data only on 2007-02-01 and 2007-02-02
+df.power$Date = as.Date(df.power$Date, format="%d/%m/%Y")
+startDate = as.Date("01/02/2007", format="%d/%m/%Y")
+endDate = as.Date("02/02/2007", format="%d/%m/%Y")
+df.power = df.power[df.power$Date >= startDate & df.power$Date <= endDate, ]
+
+## Creating the plot
+png(filename="plot1.png", width=480, height=480)
+hist(df.power$Global_active_power, main="Global Active Power",
+     xlab="Global Active Power (kilowatts)", col="red")
+dev.off()
 
