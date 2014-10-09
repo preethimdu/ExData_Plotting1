d8d16f993f8d7d22e8d1ce824bdf891788424b08
preethimdu <jp.preethika@gmail.com>
2014-10-09 09:04:50 +0530

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -2,6 +2,8 @@
 ##
 ## plot2.R - generates plot2.png
 
+
+
 ## First of all, we make sure we have the downloaded data available, we will
 ## put it in a file in the local working directory
 filename = "exdata_plotting1.zip"
